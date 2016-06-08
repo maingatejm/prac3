e60effb553301962c7d3a30ef07746c01056f8cb
maingate_choi <ttbj123@gmail.com>
2016-06-08 13:17:49 +0900

    댓글수정

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
 	list_display = ['title', 'content_size', 'created_at']
 	def content_size(self, post):
 		return '%s자' %len(post.content)
 
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ['post', 'user', 'message']
+
+
 admin.site.register(Post,PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from .forms import CommentForm
 
 def index(request):
@@ -22,3 +22,18 @@ def comment_new(request, post_pk):
 	else : 
 		form = CommentForm()
 	return render(request, 'blog/comment_form.html', {'form':form,})
+
+def comment_edit(request, post_pk, pk):
+		comment = get_object_or_404(Comment, pk = pk)
+		if request.method == 'POST':
+			form = CommentForm(request.POST, instance=comment)
+			if form.is_valid():
+				comment = form.save(commit=False)
+				comment.post = get_object_or_404(Post, pk=post_pk)
+				comment.user = request.user
+				comment.save()
+
+			return redirect('blog:post_detail', post_pk)
+		else:
+			form = CommentForm(instance=comment)
+		return render(request,'blog/comment_form.html', {'form':form,})
