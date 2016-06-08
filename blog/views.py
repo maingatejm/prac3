fe55013bef9bbf8a1cb2b2ec72f3790796d6b7ba
maingate_choi <ttbj123@gmail.com>
2016-06-08 12:38:14 +0900

    index와 post_detail 간 링크, 오타수정

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from .model import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def index(request):
 	post_list = Post.objects.all()
-	return render(request, 'blog/index.html', {'post':post,})
+	return render(request, 'blog/index.html', {'post_list':post_list,})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':post,})
